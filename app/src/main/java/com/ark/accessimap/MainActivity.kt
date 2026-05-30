4e5e307959ed8ce2aeb1dd5c2a3bbe673d675921
ProgramminCat <72707293+ProgramminCat@users.noreply.github.com>
2026-05-30 16:12:32 -0400

    bottom sheet thingy start

--- app/src/main/java/com/ark/accessimap/MainActivity.kt
+++ app/src/main/java/com/ark/accessimap/MainActivity.kt
@@ -3,9 +3,11 @@ package com.ark.accessimap
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -14,13 +16,19 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -29,9 +37,11 @@ import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.ark.accessimap.ui.theme.AccessimapTheme
+import kotlinx.coroutines.launch
 import org.maplibre.android.style.expressions.Expression.image
 import org.maplibre.compose.camera.CameraPosition
 import org.maplibre.compose.camera.rememberCameraState
@@ -53,6 +63,7 @@ import org.maplibre.compose.map.OrnamentOptions
 import org.maplibre.compose.sources.GeoJsonData
 import org.maplibre.compose.sources.rememberGeoJsonSource
 import org.maplibre.compose.style.BaseStyle
+import org.maplibre.compose.util.ClickResult
 import org.maplibre.spatialk.geojson.GeoJson
 import org.maplibre.spatialk.geojson.Position
 import org.maplibre.spatialk.units.extensions.meters
@@ -120,8 +131,13 @@ enum class AppDestinations(
     PROFILE("Profile", Icons.Default.AccountBox),
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Map(modifier: Modifier = Modifier) {
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+
     val cameraState = rememberCameraState(
         firstPosition = CameraPosition(
             target = Position(
@@ -190,8 +206,42 @@ fun Map(modifier: Modifier = Modifier) {
             source = poisSource,
             iconImage = image(locationMarkerIcon, drawAsSdf = true),
             iconAnchor = const(SymbolAnchor.Center),
-            iconAllowOverlap = const(true)
+            iconAllowOverlap = const(true),
+            onClick = { features ->
+                if (features.isNotEmpty()) {
+                    val clickedFeature = features[0]
+
+                    Log.d("Accessimap", clickedFeature.properties.toString())
+
+                    showBottomSheet = true
+                }
+
+                ClickResult.Consume
+            }
         )
+    }
+
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            sheetState = sheetState
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text("Random Place\n67 Random Street")
+
+                Button(
+                    onClick = {
+                        scope.launch {
+                            sheetState.hide()
+                        }.invokeOnCompletion {
+                            showBottomSheet = false
+                        }
+                    }
+                ) {
+                    Text("Close")
+                }
+            }
+        }
     }
 }
 
