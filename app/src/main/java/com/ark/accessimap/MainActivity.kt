111cb240f7952ed24d7dbf2547a9ef99c5d6bd7f
ProgramminCat <72707293+ProgramminCat@users.noreply.github.com>
2026-06-02 19:57:30 -0400

    search bar

--- app/src/main/java/com/ark/accessimap/MainActivity.kt
+++ app/src/main/java/com/ark/accessimap/MainActivity.kt
@@ -9,13 +9,17 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.List
@@ -25,8 +29,14 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBarColors
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -37,10 +47,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -149,6 +161,7 @@ fun Map(modifier: Modifier = Modifier) {
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
     var selectedPoi by remember { mutableStateOf("") }
+    var searchText by remember { mutableStateOf("") }
 
     LaunchedEffect(Unit) {
         val client = OkHttpClient()
@@ -185,50 +198,79 @@ fun Map(modifier: Modifier = Modifier) {
 
     val locationMarkerIcon = rememberVectorPainter(Icons.Default.LocationOn)
 
-    MaplibreMap(
-        baseStyle = BaseStyle.Uri("https://tiles.openfreemap.org/styles/liberty"),
-        cameraState = cameraState,
-        options = MapOptions(
-            ornamentOptions = OrnamentOptions(
-                isLogoEnabled = false,
-                isAttributionEnabled = true,
-                isScaleBarEnabled = false,
-            )
-        )
-    ) {
-        LocationPuck(
-            idPrefix = "user",
-            location = locationState.location,
-            bearing = locationState.mostAccurateBearing(),
+    Box(modifier = Modifier.fillMaxSize()) {
+        MaplibreMap(
+            baseStyle = BaseStyle.Uri("https://tiles.openfreemap.org/styles/liberty"),
             cameraState = cameraState,
-        )
+            options = MapOptions(
+                ornamentOptions = OrnamentOptions(
+                    isLogoEnabled = false,
+                    isAttributionEnabled = true,
+                    isScaleBarEnabled = false,
+                )
+            )
+        ) {
+            LocationPuck(
+                idPrefix = "user",
+                location = locationState.location,
+                bearing = locationState.mostAccurateBearing(),
+                cameraState = cameraState,
+            )
 
-        LocationTrackingEffect(locationState = locationState) {
-            val position = currentLocation.location?.position?.value
-            if (position != null) {
-                cameraState.animateTo(CameraPosition(target = position, zoom = 15.0))
+            LocationTrackingEffect(locationState = locationState) {
+                val position = currentLocation.location?.position?.value
+                if (position != null) {
+                    cameraState.animateTo(CameraPosition(target = position, zoom = 15.0))
+                }
             }
+
+            val poisSource = rememberGeoJsonSource(
+                GeoJsonData.JsonString(
+                    poisJson ?: "{\"type\":\"FeatureCollection\",\"features\":[]}"
+                )
+            )
+
+            SymbolLayer(
+                id = "pois-layer",
+                source = poisSource,
+                iconImage = image(locationMarkerIcon, drawAsSdf = true),
+                iconAnchor = const(SymbolAnchor.Center),
+                iconAllowOverlap = const(true),
+                onClick = { features ->
+                    if (features.isNotEmpty()) {
+                        selectedPoi = features[0].properties.toString()
+                        showBottomSheet = true
+                    }
+
+                    ClickResult.Consume
+                }
+            )
         }
 
-        val poisSource = rememberGeoJsonSource(
-            GeoJsonData.JsonString(poisJson ?: "{\"type\":\"FeatureCollection\",\"features\":[]}")
-        )
-
-        SymbolLayer(
-            id = "pois-layer",
-            source = poisSource,
-            iconImage = image(locationMarkerIcon, drawAsSdf = true),
-            iconAnchor = const(SymbolAnchor.Center),
-            iconAllowOverlap = const(true),
-            onClick = { features ->
-                if (features.isNotEmpty()) {
-                    selectedPoi = features[0].properties.toString()
-                    showBottomSheet = true
-                }
-
-                ClickResult.Consume
-            }
-        )
+        Surface(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(30.dp)
+                .fillMaxWidth(),
+            color = Color.White,
+            shadowElevation = 4.dp,
+            shape = RoundedCornerShape(24.dp)
+        ) {
+            TextField(
+                value = searchText,
+                onValueChange = { searchText = it },
+                placeholder = { Text("Search places...") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(16.dp),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent
+                )
+            )
+        }
     }
 
     if (showBottomSheet) {
