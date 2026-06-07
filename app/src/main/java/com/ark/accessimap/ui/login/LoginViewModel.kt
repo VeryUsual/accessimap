76b4e65668f303170840a263c95de62b25c5858d
ProgramminCat <72707293+ProgramminCat@users.noreply.github.com>
2026-06-07 15:23:43 -0400

    small changes

--- app/src/main/java/com/ark/accessimap/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/ark/accessimap/ui/login/LoginViewModel.kt
@@ -18,7 +18,6 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
     val loginResult: LiveData<LoginResult> = _loginResult
 
     fun login(username: String, password: String) {
-        // can be launched in a separate asynchronous job
         val result = loginRepository.login(username, password)
 
         if (result is Result.Success) {
@@ -39,7 +38,7 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
         }
     }
 
-    // A placeholder username validation check
+    // username validation check
     private fun isUserNameValid(username: String): Boolean {
         return if (username.contains('@')) {
             Patterns.EMAIL_ADDRESS.matcher(username).matches()
@@ -48,8 +47,8 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
         }
     }
 
-    // A placeholder password validation check
+    // password validation check
     private fun isPasswordValid(password: String): Boolean {
-        return password.length > 5
+        return password.length > 3
     }
 }
